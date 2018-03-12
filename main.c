b6860bddb40cb9d3d7f518e4c6b255e39e20e79b
Nathan Cooper <nac33@students.uwf.edu>
2018-03-12 14:16:22 -0500

    Add handling user input for main menu so users may select any of the options available to them.

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 
 void start();
 void displayMainMenu();
-void handleCreateUser();
+void handleInput(int);
+void displayCreateUser();
 
 int main(int argc, char** argv)
 {
@@ -19,23 +20,29 @@ void start()
 
     int ans = 0;
     puts("How might I help you today?");
-    displayMainMenu();
     char buffer[MAX_RESPONSE];
-    fgets(buffer, sizeof(buffer), stdin);
-    int result = sscanf(buffer, "%d", &ans);
-    if (result < 1)
+    while (1)
     {
 
-        puts("You did not input a number. You must put a number representing one of the options. Ex: For option 1 you would type \"1\"");
         displayMainMenu();
+        fgets(buffer, sizeof(buffer), stdin);
+        int result = sscanf(buffer, "%d", &ans);
+        if (result > 0)
+        {
+
+            printf("User inputted: %d\n", ans);
+            handleInput(ans);
+
+        }
+        else
+        {
+
+            puts("You did not input a number. You must put a number representing one of the options. Ex: For option 1 you would type \"1\"");
+
+        }
 
     }
-    else
-    {
 
-        printf("User inputed: %d\n", ans);
-
-    }
 
 }
 
@@ -44,6 +51,38 @@ void displayMainMenu()
 
     puts("1. Create User");
     puts("2. Login");
+    puts("3. Exit")
     puts("Please select one of the above options: ");
+
+}
+
+void handleInput(int option)
+{
+
+    if (option == 1)
+    {
+
+        puts("Creating user...");
+
+    }
+    else if (option == 2)
+    {
+
+        puts("Logging in...");
+
+    }
+    else if (option == 3)
+    {
+
+        puts("Exiting...");
+        exit(0);
+
+    }
+    else
+    {
+
+        puts("Invalid option selected...");
+
+    }
 
 }
