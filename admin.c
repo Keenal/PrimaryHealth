a580004950632a353f738cec6fa17fc7f70d07bf
Nathan Cooper <nac33@students.uwf.edu>
2018-04-01 10:36:42 -0500

    Moved admin functionality into separate class

--- admin.c
+++ admin.c
@@ -1,1 +1,18 @@
 
+
+void adminMain()
+{
+
+    while(1)
+    {
+
+        puts("1. Create a patient.");
+        puts("2. Delete a patient.");
+        puts("3. Logout.");
+
+        int adminOption;
+        
+
+    }
+
+}
